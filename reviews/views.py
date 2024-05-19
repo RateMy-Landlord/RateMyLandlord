4629b9ee9c4763bc345ee6bb088562abe10e22f9
Dae❤️ <74119677+daeisbae@users.noreply.github.com>
2024-05-19 01:54:31 -0700

    Landlord Detail Page Work

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Reviews
+from landlord.models import Landlord
 
-# Create your views here.
+def landlord_detail(request, landlord_id):
+    landlord = get_object_or_404(Landlord, pk=landlord_id)
+    reviews = Reviews.objects.filter(lord=landlord)
+    return render(request, 'landlord_detail.html', {'landlord': landlord, 'reviews': reviews})
